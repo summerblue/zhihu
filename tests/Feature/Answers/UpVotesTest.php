5fd48825b56c54a3cd55a31f484ffde414b6864a
nofirst <1025926969@qq.com>
2020-07-20 15:04:55 +0000

    vote only once

--- tests/Feature/Answers/UpVotesTest.php
+++ tests/Feature/Answers/UpVotesTest.php
@@ -46,4 +46,21 @@ class UpVotesTest extends TestCase
 
         $this->assertCount(0, $answer->refresh()->votes('vote_up')->get());
     }
+
+    /** @test */
+    public function can_vote_up_only_once()
+    {
+        $this->signIn();
+
+        $answer = create(Answer::class);
+
+        try {
+            $this->post("/answers/{$answer->id}/up-votes");
+            $this->post("/answers/{$answer->id}/up-votes");
+        } catch (\Exception $e) {
+            $this->fail('Can not vote up twice.');
+        }
+
+        $this->assertCount(1, $answer->refresh()->votes('vote_up')->get());
+    }
 }
