ab92c3c3d09e72278f2283aca3047e630239dec8
nofirst <1025926969@qq.com>
2020-07-20 14:22:31 +0000

    include sudo-su

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,21 +6,13 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
+        if (config('app.debug')) {
+            $this->app->register('VIACreative\SudoSu\ServiceProvider');
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         //
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,4 +30,9 @@
     @include('layouts._footer')
 </div>
 </body>
+
+@if (config('app.debug'))
+    @include('sudosu::user-selector')
+@endif
+
 </html>
