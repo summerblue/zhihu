3e949dae4659177292c65362225b57c0d55e3693
nofirst <1025926969@qq.com>
2020-07-20 18:07:00 +0000

    invite someone

--- app/Http/Controllers/PublishedQuestionsController.php
+++ app/Http/Controllers/PublishedQuestionsController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
-use Carbon\Carbon;
+use App\Models\User;
+use App\Notifications\YouWereInvited;
 use Illuminate\Http\Request;
 
 class PublishedQuestionsController extends Controller
@@ -16,6 +17,21 @@ class PublishedQuestionsController extends Controller
     public function store(Question $question)
     {
         $this->authorize('update', $question);
+
+        // Inspect the body of the reply for the username mentions
+        preg_match_all('/@([^\s.]+)/',$question->content,$matches);
+
+        $names = $matches[1];
+
+        // And then notify user
+        foreach ($names as $name){
+            $user = User::whereName($name)->first();
+
+            if($user){
+                $user->notify(new YouWereInvited($question));
+            }
+        }
+
 
         $question->publish();
     }
---
+++ app/Notifications/YouWereInvited.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class YouWereInvited extends Notification
+{
+    use Queueable;
+
+    private $question;
+
+    public function __construct($question)
+    {
+        $this->question = $question;
+    }
+
+    public function via()
+    {
+        return ['database'];
+    }
+
+    public function toArray($notifiable)
+    {
+        $questionCreator = $this->question->creator;
+
+        return [
+            'user_id' => $questionCreator->id,
+            'user_name' => $questionCreator->name,
+            'question_id' => $this->question->id,
+            'question_title' => $this->question->title,
+        ];
+    }
+}
