94898a40829d140a42b5ce02384f5c174ca9698d
Summer <summer@learnku.com>
2021-03-24 09:52:31 +0000

    post answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 
 class AnswersController extends Controller
 {
-    public function store(Question $question)
+    public function store($questionId)
     {
+        $question = Question::published()->findOrFail($questionId);
+
         $question->answers()->create([
             'user_id' => request('user_id'),
             'content' => request('content')
--- tests/Feature/PostAnswersTest.php
+++ tests/Feature/PostAnswersTest.php
@@ -12,9 +12,9 @@ class PostAnswersTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function user_can_post_an_answer_to_a_question()
+    public function user_can_post_an_answer_to_a_published_question()
     {
-        $question = Question::factory()->create();
+        $question = Question::factory()->published()->create();
         $user = User::factory()->create();
 
         $response = $this->post("/questions/{$question->id}/answers", [
@@ -28,5 +28,23 @@ class PostAnswersTest extends TestCase
         $this->assertNotNull($answer);
 
         $this->assertEquals(1,$question->answers()->count());
+    }
+
+    /** @test */
+    public function can_not_post_an_answer_to_an_unpublished_question()
+    {
+        $question = Question::factory()->unpublished()->create();
+        $user = User::factory()->create();
+
+        $response = $this->withExceptionHandling()
+            ->post("/questions/{$question->id}/answers", [
+                'user_id' => $user->id,
+                'content' => 'This is an answer.'
+            ]);
+
+        $response->assertStatus(404);
+
+        $this->assertDatabaseMissing('answers',['question_id' => $question->id]);
+        $this->assertEquals(0, $question->answers()->count());
     }
 }
